a78854310ba29dd12a2f5f1534e3d06e56ae669c
corshidan <corshi.dan@gmail.com>
2021-09-12 14:52:04 +0100

    added structure to GamePage and some styling to help visualize

--- src/components/GamePage/index.js
+++ src/components/GamePage/index.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
-import initialCards from "../../cardsData.js";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import initialCards from '../../cardsData.js';
+import { Link } from 'react-router-dom';
 
 export default function GamePage({ user, balance }) {
-  const [cards, setCards] = useState(initialCards);
+	const [cards, setCards] = useState(initialCards);
 
-  return (
-    <div>
-      <p>{user} {balance}</p>
-      <button onClick={() => console.log(user, balance)}>Click</button>
-      <button>
-        <Link to="/">Back to main</Link>
-      </button>
-    </div>
-  );
+	return (
+		<div className="game">
+			<div className="topBar">
+				<button>
+					<Link to="/">Back to main</Link>
+				</button>
+				<p className="displayName">
+					<p>{user}</p>
+					<p>{balance}</p>
+				</p>
+			</div>
+			<div className="mainGame">
+				<div className="main">
+					<div className="dealer"></div>
+					<div className="player">
+						<div className="box">
+							<p className="score">score</p>
+							<div className="betAmount">Bet Amount</div>
+							<button>Clear</button>
+						</div>
+					</div>
+				</div>
+				<div className="amountSelector"></div>
+			</div>
+		</div>
+	);
 }
